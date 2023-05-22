8d06481af4b84e38b555c0a1603bc4b90b9daa9d
AhmedSaIah <ahmedsalah5446@gmail.com>
2023-05-22 15:03:09 +0300

    search fix & prettier

--- src/App.js
+++ src/App.js
@@ -1,55 +1,66 @@
-import { React, useEffect, useState } from 'react'
-import './App.css'
-import SearchIcon from './search.svg'
-import MovieCard from './MovieCard';
+import { React, useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
-const apiKey = '5e145d85';
-const API_URL = `https://www.omdbapi.com?apikey=${apiKey}`
+const apiKey = "5e145d85";
+const API_URL = `https://www.omdbapi.com?apikey=${apiKey}`;
 
 const App = () => {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
-        setMovies(data.Search);
-    }
+    setMovies(data.Search);
+  };
 
-    useEffect(() => {
-        searchMovies('Pulp fiction');
-    }, []);
+  useEffect(() => {
+    searchMovies("Pulp fiction");
+  }, []);
 
-    return (
-        <div className="app">
-            <h1>CinemaCorner</h1>
-            <div className="search" id="myBtn">
-                <input
-                    placeholder="Search for movies"
-                    value={searchTerm}
-                    onChange={(e) => { setSearchTerm(e.target.value) }}
-                />
-                <img
-                    src={SearchIcon}
-                    alt="search"
-                    onClick={() => { searchMovies(searchTerm) }}
-                />
-            </div>
-            {movies?.length > 0
-                ? (
-                    <div className="container">
-                        {movies.map((movie) => (
-                            <MovieCard movie={movie} />
-                        ))}
-                    </div>
-                ) : (
-                    <div className="empty">
-                        <h2>No movies found</h2>
-                    </div>
-                )}
+  return (
+    <div className="app">
+      <h1>CinemaCorner</h1>
+      <div
+        className="search"
+        id="myBtn"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            searchMovies(searchTerm);
+          }
+        }}
+      >
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => {
+            searchMovies(searchTerm);
+          }}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
-    );
-}
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
